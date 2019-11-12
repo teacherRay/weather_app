262952a8cca1951bd9705786c969d1ef3c1823d4
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-12 14:30:15 +0700

    working weather api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #https://www.youtube.com/watch?v=D8-snVfekto
 #https://github.com/teacherRay/weather_app
 
+#463b0ae259c10982f0e5d0dccf54bde5
+
 import requests
 import tkinter as tk
 
@@ -12,6 +14,17 @@ HEIGHT = 500
 WIDTH = 600
 
 root = tk.Tk()
+
+def test_function(entry):
+    print (entry)
+
+def get_weather(city):
+    weather_key = '463b0ae259c10982f0e5d0dccf54bde5'   
+    url ='https://api.openweathermap.org/data/2.5/forecast' 
+    params = {'APPID': weather_key,'q': city, 'units': 'metric'}
+    response = requests.get(url, params=params)
+    print (response.json())
+
 
 canvas=tk.Canvas(root, height = HEIGHT, width = WIDTH)
 canvas.pack()
@@ -25,7 +38,7 @@ frame.place(relx = 0.5, rely = 0.1, relwidth = 0.75, relheight = 0.1, anchor = '
 entry = tk.Entry(frame, font = 40)
 entry.place(relwidth = 0.65, relheight = 1)
 
-button = tk.Button(frame, text = "Get Weather", bg = PALEBLUE, font = 30)
+button = tk.Button(frame, text = "Get Weather", font = 30, command = lambda:get_weather(entry.get()))
 button.place(relx =.67,rely = 0, relwidth = 0.32, relheight = 1)
 
 lower_frame = tk.Frame(root, bg = DARKBLUE,bd = 3)
