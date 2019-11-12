87be147b0d991ae578acea9387f4bb458f210d4c
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-12 11:50:39 +0700

    frames and button, label and entry

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ import tkinter as tk
 from tkinter import messagebox
 root = tk.Tk()
 
+LIGHTBLUE = "#03bafc"
+DARKBLUE = "#034efc"
+PALEBLUE = "#c1f0f5"
+
 #def helloCallBack():
 #       msg = messagebox.showinfo( "Hello Python", "Hello World")
 canvas = tk.Canvas(root, height = 400, width = 600)
 canvas.pack()
 
-frame = tk.Frame(root, bg= "pink")
+frame = tk.Frame(root, bg = LIGHTBLUE)
 frame.place(relx = 0.1, rely = 0.1, relheight = 0.8, relwidth = 0.8)
 
-button = tk.Button(root, text = "test button", bg = "red", fg = "yellow")
+button = tk.Button(frame, text = "Test Button", bg = DARKBLUE, fg = PALEBLUE)
+label = tk.Label(frame, text = "My lovely label", bg = DARKBLUE, fg = PALEBLUE)
+entry = tk.Entry(frame, text = "My entry", bg = DARKBLUE, fg = PALEBLUE)
+
 button.pack()
+label.pack()
+entry.pack()
 
 root.mainloop()
