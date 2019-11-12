78ae90bedb0a8e0e2b3fb3f03c1fbc1ab8dabfb3
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-12 13:50:08 +0700

    with background image

--- main.py
+++ main.py
@@ -8,27 +8,30 @@ LIGHTBLUE = "#03bafc"
 DARKBLUE = "#034efc"
 PALEBLUE = "#c1f0f5"
 
-HEIGHT = 700
-WIDTH = 800
+HEIGHT = 500
+WIDTH = 600
 
 root = tk.Tk()
 
-canvas=tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas=tk.Canvas(root, height = HEIGHT, width = WIDTH)
 canvas.pack()
 
-frame = tk.Frame(root, bg = DARKBLUE,bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor = 'n')
+background_image = tk.PhotoImage(file ='landscape.png')
+background_label = tk.Label(root, image = background_image)
+background_label.place(relwidth = 1, relheight = 1)
+frame = tk.Frame(root, bg = DARKBLUE, bd = 3)
+frame.place(relx = 0.5, rely = 0.1, relwidth = 0.75, relheight = 0.1, anchor = 'n')
 
 entry = tk.Entry(frame, font = 40)
-entry.place(relwidth=0.65, relheight=1)
+entry.place(relwidth = 0.65, relheight = 1)
 
 button = tk.Button(frame, text = "Get Weather", bg = PALEBLUE, font = 30)
-button.place(relx=.67,rely=0, relwidth=0.32, relheight=1)
+button.place(relx =.67,rely = 0, relwidth = 0.32, relheight = 1)
 
-lower_frame = tk.Frame(root, bg = DARKBLUE,bd=8)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor = 'n')
+lower_frame = tk.Frame(root, bg = DARKBLUE,bd = 3)
+lower_frame.place(relx = 0.5, rely = 0.25, relwidth = 0.75, relheight = 0.6, anchor = 'n')
 
 label = tk.Label(lower_frame, font = 40)
-label.place(relwidth=1, relheight=1)
+label.place(relwidth = 1, relheight = 1)
 
 root.mainloop()
