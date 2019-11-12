b3d2a9600c8901c2fccec85ad40f19f856bb58b6
teacherRay <34903301+teacherRay@users.noreply.github.com>
2019-11-12 11:22:14 +0700

    added frame

--- main.py
+++ main.py
@@ -3,8 +3,16 @@
 
 import requests
 import tkinter as tk
-
+from tkinter import messagebox
 root = tk.Tk()
+
+#def helloCallBack():
+#       msg = messagebox.showinfo( "Hello Python", "Hello World")
+canvas = tk.Canvas(root, height = 400, width = 600)
+canvas.pack()
+
+frame = tk.Frame(root, bg= "pink")
+frame.place(relx = 0.1, rely = 0.1, relheight = 0.8, relwidth = 0.8)
 
 button = tk.Button(root, text = "test button", bg = "red", fg = "yellow")
 button.pack()
